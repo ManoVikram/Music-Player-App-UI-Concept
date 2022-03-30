e8bfa56d29a1f2576fb59b95dd1ffaf3c1cbff8e
Mano Vikram <manovik18@gmail.com>
2022-03-30 19:43:30 +0530

    Initial commit!

---
+++ android/app/src/main/kotlin/com/example/music_concept_ui_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.music_concept_ui_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Music Concept UI",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      home: const MusicConceptUI(),
+    );
+  }
+}
+
+class MusicConceptUI extends StatelessWidget {
+  const MusicConceptUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
